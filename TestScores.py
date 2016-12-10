0dd6518f1d5fb933fc5b7e7c118f9ee2b76178d9
ceneri <ceneri@ucsc.edu>
2016-12-10 11:54:18 -0800

    Multiple sets of scores are taken now, and program repeats until user chooses to exit

--- TestScores.py
+++ TestScores.py
@@ -1,30 +1,52 @@
 #Cesar Neri
-#December 9, 2016
+#December 10, 2016
 
-#Excercise 2-2 from Murach's Python Programming Book
+#Excercise 3-2 from Murach's Python Programming Book
 
-#Get 3 scores, prin them, the total and the average
+#Get sets a set of scores, as many as the user wants, print the total and their average
 
 #!/usr/bin/env python3
 
 #Greeting
 print("Test Scores Program")
 
-#Get user data
-print("\nEnter 3 test scores")
-print("========================================")
-score1  = int(input("Enter Test Score:\t\t"))
-score2  = int(input("Enter Test Score:\t\t"))
-score3  = int(input("Enter Test Score:\t\t"))
-print("========================================")
+#Initial response
+response = "y"
 
-#Calculate Output
-totalScore = score1 + score2 + score3
-averageScore = totalScore/3
+#Check user response (run program more than once)
+while response.lower() == "y":
 
-#Print output
-print ("Your Scores:\t" + str(score1), str(score2), str(score3) )
-print ("Total Score: \t" + str(totalScore) )
-print ("Average Score: \t" + str(averageScore) )
+    #Clear score and count
+    totalScore = 0
+    count = 0
+
+    #Get user data
+    print("\nEnter a set of scores, enter 'end' to end input\n========================================")
+    
+    #Get more than one test score
+    while True:
+        score  = input("Enter Test Score:\t\t")
+        
+        #validate score
+        if score.lower() == "end":
+            break
+        elif int(score) > 0 and int(score) <= 100:  #validate score range
+            totalScore += int(score)
+            count += 1
+        else:
+            print("Test score must be within 0 and 100, please try again.")
+        
+        
+    print("========================================")
+
+    #Calculate Output
+    averageScore = round(totalScore/count, 2)
+
+    #Print output
+    print ("Total Score: \t" + str(totalScore) )
+    print ("Average Score: \t" + str(averageScore) )
+
+    #Get new response
+    response = input("Enter another set of scores (y/n)?\t")
 
 print("\nGoodbye!")
