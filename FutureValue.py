33f97d74e8f4abaf4a702beea4db1f32349ab500
ceneri <ceneri@ucsc.edu>
2016-12-10 15:18:45 -0800

    Program now implements functions

--- FutureValue.py
+++ FutureValue.py
@@ -1,64 +1,88 @@
 #Cesar Neri
 #December 10, 2016
 
-#Excercise 3-3 from Murach's Python Programming Book
+#Excercise 4-1 from Murach's Python Programming Book (Future value calculator)
 
-#Future value calculator
+#Function main(), get_float(), and get_int() defined.
+#main() called only if it is the main module
+
+
 
 #!/usr/bin/env python3
 
-#Greeting
-print("Welcome to the Future Value Calculator\n")
+#MAIN DEFINITION-------------------------------------------------------------
 
-#Initial response
-response = "y"
-
-#Allow multiple runs of the program
-while response.lower() == "y":
-
-    print()
-
-    #Get Monthly investment
-    while True:
-        monthlyInvestment = round(float(input("Enter monthly investment:\t")), 2)
-        if monthlyInvestment > 0:
-            break
-        else:
-            print ("Entry must be greater than 0. Please try again.")
-        
-    #Get yearlly interest rate
-    while True:
-        yearlyRate = int(input("Enter yearly interest rate:\t"))
-        if yearlyRate > 0 and yearlyRate <= 15:
-            break
-        else:
-            print ("Entry must be greater than 0 and less than or equal to 15. Please try again.")
+def main():
     
-    #Get yearly interest rate
+    #Greeting
+    print("Welcome to the Future Value Calculator\n")
+
+    #Initial response
+    response = "y"
+
+    #Allow multiple runs of the program
+    while response.lower() == "y":
+
+        print()
+
+        #Get Monthly investment
+        monthlyInvestment = get_float("Enter monthly investment:\t", 0, 1000)
+        
+        #Get yearlly interest rate
+        yearlyRate = get_float("Enter yearly interest rate:\t", 0, 15)
+               
+        #Get yearly interest rate
+        years = get_int("Enter number of years:\t\t", 0, 50)
+
+        #Calculate
+        total = 0.0
+        monthlyRate = yearlyRate/12/100
+        print()
+                   
+        #Print result
+        for i in range(1, years+1):
+            #calculte each month
+            for j in range (12):
+                total += monthlyInvestment
+                total += round(total*monthlyRate, 3)
+        
+            print("Year = " + str(i) +  "\tFuture Value = " + str( round(total,2) ) )
+                   
+
+        #Get new response
+        response = input("\nContinue (y/n)?\t")
+
+    print("\nGoodbye!")
+    
+
+#HELPER FUNCTIONS-------------------------------------------------------------
+
+
+#Takes a prompt argument to get input from user, it is then transformed to float and returned
+def get_float(prompt, low_val, high_val):
     while True:
-        years = int(input("Enter number of years:\t\t"))
-        if years > 0 and years <= 50:
+        fNumber = float(input(prompt));
+        if fNumber > low_val and fNumber <= high_val:
             break
         else:
-            print ("Entry must be greater than 0 and less than or equal to 50. Please try again.")
+            print("Entry must be greater than " + str(low_val) + " and less than or equal to " + str(high_val) + ". Please try again.")
 
+    return fNumber
 
-    #Calculate
-    total = 0.0
-    monthlyRate = yearlyRate/12/100
-    print()
-                   
-    #Print result
-    for i in range(1, years+1):
-        #calculte each month
-        for j in range (12):
-            total += monthlyInvestment
-            total += round(total*monthlyRate, 3)
-        
-        print("Year = " + str(i) +  "\tFuture Value = " + str( round(total,2) ) )
-                   
+#Takes a prompt argument to get input from user, it is then transformed to int and returned
+def get_int(prompt, low_val, high_val):
+    while True:
+        fNumber = int(input(prompt));
+        if fNumber > low_val and fNumber <= high_val:
+            break
+        else:
+            print("Entry must be greater than " + str(low_val) + " and less than or equal to " + str(high_val) + ". Please try again.")
 
-    #Get new response
-    response = input("\nContinue (y/n)?\t")
+    return fNumber
+    
 
-print("\nGoodbye!")
+#CALL TO MAIN-------------------------------------------------------------------------
+
+#If started as the main module, call the main() function
+if __name__ == "__main__":
+    main()
