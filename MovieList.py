96248d2805b21ed075cede22294cc3d1f36fbd50
ceneri <ceneri@ucsc.edu>
2016-12-13 09:36:59 -0800

    Now using a 2D list

--- MovieList.py
+++ MovieList.py
@@ -1,6 +1,8 @@
 #Cesar Neri
 #December 11, 2016
 
+#Movie List uses a 2D list to keep movie information, and display it.
+
 #!/usr/bin/env python3
 
 #display available options
@@ -12,26 +14,33 @@ def display_menu():
 
 #List the movies in the parameter list    
 def list_movies(movie_list):
-    for i in range(0, len(movie_list)):
-        print(str(i+1) + ". " + str(movie_list[i]))
+    count = 1
+    for item in movie_list:
+        print(str(count) + ". " + item[0] + " (" + str(item[1]) + ")" )
+        count += 1
+    
     print()
 
 #Adds a new movie to the parameter list        
 def add_movie(movie_list):
     movie_title = input("Name:\t")
-    movie_list.append(movie_title)
+    movie_year = input("Year:\t")
+    new_movie = [movie_title, movie_year]
+    movie_list.append(new_movie)
+    print(movie_title + " was added.\n")
 
 #removes a movie from the parameter list  
 def del_movie(movie_list):
     movie_number = int(input("Number:\t"))
     if movie_number > 0 and movie_number <= len(movie_list):
-        movie_list.pop(movie_number-1)
+        movie_del = movie_list.pop(movie_number-1)
+        print(movie_del[0] + " was deleted.\n")
     else:
         print("Invalid movie number, please try again")
         
 #MAIN
 def main():
-    movies = ["Monty Python and the Holy Grail", "On the Waterfront", "2012"]
+    movies = [ ["Monty Python and the Holy Grail", 1975], ["On the Waterfront", 1954], ["2012", 2012] ]
 
     display_menu()
 
